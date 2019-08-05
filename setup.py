0c2e9a711cbbba82ae4f97785357fe4fd484747c
Matěj Týč <matej.tyc@gmail.com>
2019-08-05 16:05:17 +0200

    Added other packages besides the root one.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EnterpriseyIntranet/teap",
-    packages=["teap"],
+    packages=setuptools.find_packages(),
     package_dir={"teap": "src/web"},
     include_package_data=True,
     classifiers=[
