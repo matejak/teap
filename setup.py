43e716a10e7c7839043d60df101e7cc1de1b4d31
Matěj Týč <matej.tyc@gmail.com>
2019-08-05 14:47:08 +0200

    Added setup.py
    Also introduces the teap.backend package.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+SETUPDIR = os.path.dirname(__file__)
+PKGDIR = os.path.join(SETUPDIR, 'src')
+
+with open(os.path.join(SETUPDIR, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='teap',
+    version='0.0.1',
+    author='EnterpriseyIntranet',
+    description="Integration of LDAP and Nextcloud, Rocketchat",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EnterpriseyIntranet/teap",
+    packages=["teap"],
+    package_dir={"teap": "src/web"},
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        "Operating System :: OS Independent",
+    ],
+)
